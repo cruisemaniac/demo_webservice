fa32f231cbaa2b673284d27d395000f0074c2199
Firmino Changani <flowck96@gmail.com>
2018-02-16 18:24:10 +0100

    Use dict to store all the tasks and update remove functions

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect
+from datetime import datetime
+import math
 
 # Init flask
 app = Flask(__name__)
@@ -15,6 +17,10 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 allTasks = []
 
+# Generate a random integer based on current time in UTC format
+def IDgenerator():
+	return math.floor((datetime.utcnow() - datetime(1970,1,1)).total_seconds())
+
 # Home page
 @app.route('/')
 def index():
@@ -23,7 +29,12 @@ def index():
 # Create a new task
 @app.route('/task', methods=['POST'])
 def tasks():
-	allTasks.append(request.form['task'])
+	new_task = {
+		'id': IDgenerator(),
+		'task': request.form['task'],
+		'complete': False
+	}
+	allTasks.append(new_task)
 	return redirect('/', 302)
 	
 # Read a specific task
@@ -32,14 +43,24 @@ def getTask(id):
 	return id
 
 # Update a task
-@app.route('/updatetask/<id>', methods=['POST'])
+@app.route('/updatetask/<id>', methods=['GET', 'POST'])
 def updateTask(id):
-	return id
+	if request.method == 'POST':
+		return redirect('/', 302)
+
+	if request.method == 'GET':
+		return render_template('update.html', taskname = id)
 
 # Delete a task
-@app.route('/deletetask/<taskname>', methods=['GET'])
-def deleteTask(taskname):
-	allTasks.remove(taskname)
+@app.route('/deletetask/<taskID>', methods=['GET'])
+def deleteTask(taskID):
+	
+	# Iterate and remove the item acording to the id
+	for theTask in allTasks:
+		if int(taskID) == int(theTask['id']):
+			allTasks.remove(theTask)
+
+	# redirect to homepage
 	return redirect('/', 302)
 
 if __name__ == '__main__':
